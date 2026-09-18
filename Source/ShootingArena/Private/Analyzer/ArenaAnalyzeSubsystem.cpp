e1f46908494bf7761699c91ddfa69de0c2ff214c
Qusbell <01024072245a@gmail.com>
2026-09-18 21:51:23 +0900

    기록 시스템

---
+++ Source/ShootingArena/Private/Analyzer/ArenaAnalyzeSubsystem.cpp
@@ -0,0 +1,113 @@
+﻿
+#include "Analyzer/ArenaAnalyzeSubsystem.h"
+
+#include "GameFramework/Controller.h"
+#include "GameFramework/Pawn.h"
+#include "TimerManager.h"
+
+bool UArenaAnalyzeSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+    const UWorld* World = Cast<UWorld>(Outer);
+
+    // World 아니면 거르기
+    if (!World)
+    {
+        return false;
+    }
+
+    // 클라이언트 거르기
+    if (World->GetNetMode() == ENetMode::NM_Client)
+    {
+        return false;
+    }
+
+    return
+        World->WorldType == EWorldType::Game ||
+        World->WorldType == EWorldType::PIE;
+}
+
+void UArenaAnalyzeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    UWorld* World = GetWorld();
+
+    if (World == nullptr) { return; }
+
+    World->GetTimerManager().SetTimer(
+        LoopTimerHandle,
+        this,
+        &UArenaAnalyzeSubsystem::AnalyzeControllers,
+        0.5f,   // 0.5초마다
+        true    // 반복
+    );
+}
+
+void UArenaAnalyzeSubsystem::Deinitialize()
+{
+    UWorld* World = GetWorld();
+
+    if (World != nullptr)
+    {
+        World->GetTimerManager().ClearTimer(LoopTimerHandle);
+    }
+
+    UE_LOG(
+        LogTemp,
+        Warning,
+        TEXT("===== Arena Analyze Result =====")
+    );
+
+    UE_LOG(
+        LogTemp,
+        Warning,
+        TEXT("Total Samples: %d"),
+        InfoSamples.Num()
+    );
+
+    for (const FArenaInfoSample& Sample : InfoSamples)
+    {
+        UE_LOG(
+            LogTemp,
+            Log,
+            TEXT("Time: %.2f | Location: %s"),
+            Sample.TimeSeconds,
+            *Sample.Location.ToString()
+        );
+    }
+
+    UE_LOG(
+        LogTemp,
+        Warning,
+        TEXT("==============================")
+    );
+
+    Super::Deinitialize();
+}
+
+void UArenaAnalyzeSubsystem::RegisterController(AController* controller)
+{
+    if (!IsValid(controller)) { return; }
+
+	RegisteredControllers.AddUnique(controller);
+}
+
+
+void UArenaAnalyzeSubsystem::AnalyzeControllers()
+{
+    double nowTime = GetWorld()->GetTimeSeconds();
+    
+
+    for (const TWeakObjectPtr<AController>& WeakController : RegisteredControllers)
+    {
+        const AController* controller = WeakController.Get();
+        if (controller == nullptr) { continue; }
+
+        const APawn* pawn = controller->GetPawn();
+		if (pawn == nullptr) { continue; }
+
+        // 정보 수집
+        FArenaInfoSample& sample = InfoSamples.AddDefaulted_GetRef();
+        sample.TimeSeconds = nowTime;
+        sample.Location = pawn->GetActorLocation();
+	}
+}
---
+++ Source/ShootingArena/Public/Analyzer/ArenaAnalyzeSubsystem.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TimerManager.h"
+
+#include "ArenaAnalyzeSubsystem.generated.h"
+
+class AController;
+
+USTRUCT()
+struct FArenaInfoSample
+{
+    GENERATED_BODY()
+
+    UPROPERTY()
+    double TimeSeconds = 0.0;
+
+    UPROPERTY()
+    FVector Location = FVector::ZeroVector;
+};
+
+
+UCLASS()
+class SHOOTINGARENA_API UArenaAnalyzeSubsystem : public UWorldSubsystem
+{
+    GENERATED_BODY()
+
+private:
+	TArray<TWeakObjectPtr<AController>> RegisteredControllers;
+
+    TArray<FArenaInfoSample> InfoSamples;
+
+    FTimerHandle LoopTimerHandle;
+
+public:
+    virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    UFUNCTION(BlueprintCallable)
+    void RegisterController(AController* Controller);
+
+private:
+	void AnalyzeControllers();
+};
